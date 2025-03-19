ee97fab5da996aa99fbbec18f3078d94e15fa03d
pandino126 <re.luca95@gmail.com>
2025-03-19 17:17:31 +0100

    flex grow aggiunto

--- libreria.js
+++ libreria.js
@@ -16,27 +16,30 @@ const getLibraryCreate = function () {
 
       data.forEach((book) => {
         const card = document.createElement("div");
-        card.className = "card col-md-4";
-        card.style.width = "10rem";
+        card.className = "card col-lg-4";
+        card.style.width = "18rem";
 
         const bookImg = document.createElement("img");
         bookImg.src = book.img;
         bookImg.className = "card-img-top";
         bookImg.alt = book.title;
-
+        bookImg.style.height = "300px";
         const cardBody = document.createElement("div");
         cardBody.className = "card-body";
+        cardBody.style.display = "flex"; // Usa Flexbox
+        cardBody.style.flexDirection = "column"; // Disposizione verticale
+        cardBody.style.justifyContent = "space-between";
 
         const cardTitle = document.createElement("h3");
         cardTitle.innerText = book.title;
 
         const priceBook = document.createElement("h5");
         priceBook.innerText = `€${book.price}`;
-
+        priceBook.style.flexGrow = "1";
         const cardButton = document.createElement("a");
         cardButton.className = "btn btn-primary";
         cardButton.href = "#";
-        cardButton.innerText = "AGGIUNGI AL CARRELLO";
+        cardButton.innerText = "AGGIUNGI AL CARRELLO LESTO";
 
         cardBody.appendChild(cardTitle);
         cardBody.appendChild(priceBook);
